4bb7b90e561298603aa33c5d31aefe10136f4553
MarcosAdaSilva <marcaofly@gmail.com>
2022-11-01 23:09:01 -0300

    Inserindo dados no sistema

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -28,10 +28,15 @@ export class MomentFormComponent implements OnInit {
   get description() {
     return this.momentForm.get('description')!;
   }
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    this.momentForm.patchValue({ image: file });
+  }
   submit() {
     if (this.momentForm.invalid) {
       return;
     }
-    console.log('Enviou formulario');
+    console.log(this.momentForm.value);
   }
 }
