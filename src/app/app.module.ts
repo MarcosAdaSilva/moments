9d43df2b2ccd06eb5853e59d41067350a9cf4143
MarcosAdaSilva <marcaofly@gmail.com>
2022-11-03 23:59:13 -0300

    Trabalhando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NewMomentComponent } from './components/pages/new-moment.component';
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
-import { MessagesComponent } from './component/messages/messages.component';
+import { MessagesComponent } from './components/messages/messages.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
