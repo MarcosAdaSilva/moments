ac92b9cb2109ac45523052a48a68db132a7a9669
MarcosAdaSilva <marcaofly@gmail.com>
2022-11-04 23:14:13 -0300

    Resgatando dados do banco

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MomentService } from 'src/app/services/moment.service';
+
+import { Moment } from 'src/app/Moment';
+
+import { environment } from 'src/environments/environment';
+
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
